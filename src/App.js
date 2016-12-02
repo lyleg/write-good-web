e28f3b82c3118ce430088af22d301979b9a4e424
Lyle Garza <morezero06@gmail.com>
2016-12-01 21:28:57 -0800

    progress with entity + decorator

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Editor, EditorState, Modifier, Entity} from 'draft-js';
+import {Editor, EditorState, CompositeDecorator, Entity, SelectionState, Modifier} from 'draft-js';
 import writeGood from 'write-good'
 
 const styleMap = {
@@ -8,27 +8,92 @@ const styleMap = {
   },
 };
 
+let suggestions = []
+const SuggestionSpan = (props) => {
+  console.log(props)
+
+  return <span {...props} title = "Heeey" style={{color:'red'}}>{props.children}</span>;
+};
+
+/*const suggestionStrategy = function(contentBlock, callback){
+  console.log(suggestions)
+  suggestions.forEach((suggestion)=>{
+    callback(suggestion.index, suggestion.index + suggestion.offset, suggestion)
+  })
+}
+*/
+const suggestionStrategyByEntity = function(contentBlock, callback, contentState) {
+  contentBlock.findEntityRanges(
+    (character) => {
+      const entityKey = character.getEntity();
+      console.log(entityKey !== null)
+      return (
+        entityKey !== null &&
+        contentState.getEntity(entityKey).getType() === 'SUGGESTION'
+      );
+    },
+    callback
+  );
+}
+
+const compositeDecorator = new CompositeDecorator([
+  {
+    strategy: suggestionStrategyByEntity,
+    component: SuggestionSpan,
+  },
+]);
 
 
 class App extends Component {
   onChange = (editorState) => {
-    let recs = this.computeRecs()
-    if(recs.length >= 1){
-    // Modifier.replaceText for words flagged for removal, place with entity that has custom style and tooltip with more info
-      console.log(recs)
-      //this.setState({editorState:nextEditorState})
-    }else{
-      this.setState({editorState})
-    }
+    suggestions = this.computesuggestions(editorState)
+  /*  const newContentState = suggestions.reduce((contentState, suggestion)=>{
+      let key =  Entity.create(
+        'SUGGESTION',
+        'IMMUTABLE',
+        {suggestion: suggestion}
+      )
+      let targetRange = editorState.getSelection()
+debugger;
+      return Modifier.applyEntity(
+        contentState,
+        targetRange.merge({
+          anchorKey:suggestion.index,
+          anchorOffset:(suggestion.index + suggestion.offset)
+        }),
+        key
+      );
+    },editorState.getCurrentContent())*/
+
+    let newEditorState = suggestions.reduce((newEditorState, suggestion) =>{
+      let targetRange = editorState.getSelection()
+      .merge({
+        anchorOffset:suggestion.index,
+        focusOffset:(suggestion.index + suggestion.offset)
+      })
+      let key =  Entity.create(
+        'SUGGESTION',
+        'IMMUTABLE',
+        {suggestion: suggestion}
+      )
+      return Modifier.replaceText(
+        newEditorState.getCurrentContent(),
+        targetRange,
+        'entity',
+        null,
+        key
+      )
+    }, editorState)
+    this.setState({editorState:newEditorState})
   }
   constructor(props) {
     super(props);
-    this.state = {editorState: EditorState.createEmpty()};
+    this.state = {editorState: EditorState.createEmpty(compositeDecorator)};
 
   }
-  computeRecs(){
-    let plainText = this.state.editorState.getCurrentContent().getPlainText()
-    return writeGood(plainText)
+  computesuggestions(editorState){
+    let plainText = editorState.getCurrentContent().getPlainText()
+    return writeGood(plainText) || []
   }
   render() {
     const {editorState} = this.state;
