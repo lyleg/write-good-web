557b6105b0485957768448f86790b4ed4f564a27
Lyle Garza <morezero06@gmail.com>
2016-12-11 19:01:45 -0600

    lint stuff

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
-import {Editor, EditorState, CompositeDecorator} from 'draft-js'
-import {Map} from 'immutable'
+import {Editor, EditorState } from 'draft-js'
 import writeGood from 'write-good'
 import { Popover } from 'antd'
 import SimpleDecorator from 'draft-js-simpledecorator'
@@ -8,55 +7,6 @@ import SimpleDecorator from 'draft-js-simpledecorator'
 import './antd.css'
 import './Draft.css'
 import './gh-fork-ribbon.css'
-
-class SuggestionSpan extends Component {
-  remove(){
-  }
-  render(){
-    let {suggestion, offsetKey, children} = this.props
-    return (
-      <Popover content = {suggestion.reason}>
-        <span onClick ={this.remove} data-offset-key={offsetKey} style={styles.suggestionSpan}>{children}</span>
-      </Popover>
-    )
-  }
-};
-
-const suggestionStrategy = function(contentBlock, callback){
-  let suggestions = writeGood(contentBlock.get('text')) || []
-  suggestions.forEach(suggestion=>{
-    callback(suggestion.index, suggestion.index + suggestion.offset, {suggestion:suggestion})
-  })
-}
-
-const simpleDecorator = new SimpleDecorator(suggestionStrategy, SuggestionSpan)
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      editorState: EditorState.createEmpty(simpleDecorator)
-    };
-    this.onChange = (editorState) => this.setState({editorState});
-  }
-
-  render() {
-    const {editorState} = this.state;
-    return (
-      <div style = {{marginLeft:20}}>
-        <h1>Write Good Web</h1>
-        <div style = {styles.root}>
-          <Editor
-            style = {styles.editor}
-            spellCheck={true}
-            editorState={editorState}
-            onChange={this.onChange} />
-        </div>
-        <p>A simple web interface to <a href ="https://github.com/btford/write-good"> Write Good </a></p>
-      </div>
-    )
-  }
-}
 
 const styles = {
   editor:{
@@ -78,6 +28,56 @@ const styles = {
     backgroundColor:'#ffeee6',
     border: '1px solid #ffddcc',
     color: 666
+  }
+}
+
+class SuggestionSpan extends Component {
+  remove(){
+  }
+  render(){
+    let {suggestion, offsetKey, children} = this.props
+    return (
+      <Popover content={suggestion.reason}>
+        <span onClick={this.remove} data-offset-key={offsetKey} style={styles.suggestionSpan}>{children}</span>
+      </Popover>
+    )
+  }
+};
+
+const suggestionStrategy = function(contentBlock, callback){
+  debugger
+  let suggestions = writeGood(contentBlock.get('text')) || []
+  suggestions.forEach(suggestion=>{
+    callback(suggestion.index, suggestion.index + suggestion.offset, {suggestion:suggestion})
+  })
+}
+
+const simpleDecorator = new SimpleDecorator(suggestionStrategy, SuggestionSpan)
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      editorState: EditorState.createEmpty(simpleDecorator)
+    };
+    this.onChange = (editorState) => this.setState({editorState});
+  }
+
+  render() {
+    const {editorState} = this.state;
+    return (
+      <div style={{marginLeft:20}}>
+        <h1>Write Good Web</h1>
+        <div style={styles.root}>
+          <Editor
+            style={styles.editor}
+            spellCheck={true}
+            editorState={editorState}
+            onChange={this.onChange} />
+        </div>
+        <p>A simple web interface to <a href="https://github.com/btford/write-good"> Write Good </a></p>
+      </div>
+    )
   }
 }
 
