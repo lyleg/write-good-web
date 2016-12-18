1d92af5764d27bf04c449a220cc0167fa16ae378
Lyle Garza <morezero06@gmail.com>
2016-12-17 23:28:37 -0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["index.html","8c16b2b035575584570a7aeab5394822"],["manifest.json","3b90ef454a6447a3a29208d752d96140"],["static/css/main.e53be743.css","1eb05703406cdcee809dd674724dc4bb"],["static/js/main.a7e2e835.js","0d924b799ba49a5e8434e3243759cdea"]];
+var precacheConfig = [["index.html","657ec6af139109b270522738e91d1e55"],["manifest.json","3b90ef454a6447a3a29208d752d96140"],["static/css/main.464406c5.css","fba2a59dc3918869783886dee3b6ce61"],["static/js/main.4a3d290b.js","4cc5642c07514ab13f188b2d764a4d4e"]];
 var cacheName = 'sw-precache-v2-sw-precache-' + (self.registration ? self.registration.scope : '');
 
 
